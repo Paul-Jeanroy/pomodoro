1e5b2594328a41c0f5a602e59a7e969806654714
Paul-Jeanroy <pauljeanroy@outlook.fr>
2025-01-03 13:25:11 +0100

    V1.1.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ const PomodoroTimer = () => {
     }, []);
 
     const durations = {
-        WORK: 1 * 60,
+        WORK: 25 * 60,
         SHORT_BREAK: 5 * 60,
         LONG_BREAK: 15 * 60,
     };
