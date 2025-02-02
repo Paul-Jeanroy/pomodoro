ea08de7f0e357eb39d8be497d76d233f76a745f0
Paul Jeanroy <103569232+Paul-Jeanroy@users.noreply.github.com>
2025-02-02 23:00:00 +0100

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: "https://pomodoro.paul-jeanroy.fr", // Remplacez par l'URL de votre site
-    generateRobotsTxt: true, // Génère également un fichier robots.txt
-    sitemapSize: 7000, // Limite de liens par fichier sitemap
+    siteUrl: "", 
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
 };
